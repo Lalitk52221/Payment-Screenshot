aa810f32e2066b7679d1c4d8bb16946791f69f0e
Lalitk52221 <lalitk52221@outlook.com>
2024-06-03 19:36:18 +0530

    changes full

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Lalit kumar</h1>
-      <div className="card">
+      <h1>lalit kumar</h1>
+     <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
